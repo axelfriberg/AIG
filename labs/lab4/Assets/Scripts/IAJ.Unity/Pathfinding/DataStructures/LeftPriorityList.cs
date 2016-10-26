db59f424dbf44c6afde58304584b3bfdb30b3e4c
Shinkirou <joaoromeirasamado@outlook.com>
2016-10-26 10:07:45 +0100

    Lab 4

---
+++ labs/lab4/Assets/Scripts/IAJ.Unity/Pathfinding/DataStructures/LeftPriorityList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures {
+    public class LeftPriorityList : IOpenSet {
+        private List<NodeRecord> Open { get; set; }
+
+        public LeftPriorityList() {
+            this.Open = new List<NodeRecord>();
+        }
+        public void Initialize() {
+            this.Open.Clear();
+        }
+
+        public void Replace(NodeRecord nodeToBeReplaced, NodeRecord nodeToReplace) {
+            int i = this.Open.BinarySearch(nodeToBeReplaced);
+            if (i >= 0) {
+                this.Open[i] = nodeToReplace;
+            }
+        }
+
+        public NodeRecord GetBestAndRemove() {
+            var best = this.PeekBest();
+            this.Open.Remove(best);
+            return best;
+        }
+
+        public NodeRecord PeekBest() {
+            return Open[0];
+        }
+
+        public void AddToOpen(NodeRecord nodeRecord) {
+            //a little help here
+            //is very nice that the List<T> already implements a binary search method
+            int index = this.Open.BinarySearch(nodeRecord);
+            if (index < 0) {
+                this.Open.Insert(~index, nodeRecord);
+            }
+        }
+
+        public void RemoveFromOpen(NodeRecord nodeRecord) {
+            this.Open.Remove(nodeRecord);
+        }
+
+        public NodeRecord SearchInOpen(NodeRecord nodeRecord) {
+            return this.Open.FirstOrDefault(n => n.Equals(nodeRecord));
+        }
+
+        public ICollection<NodeRecord> All() {
+            return this.Open;
+        }
+
+        public int CountOpen() {
+            return this.Open.Count;
+        }
+    }
+}
---
+++ labs/lab4/Assets/Scripts/IAJ.Unity/Pathfinding/DataStructures/NodeHashmap.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using RAIN.Navigation.Graph;
+using Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures;
+using System;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding {
+
+    public class NodeHashmap : IClosedSet {
+
+        private Dictionary<NodeRecord, NodeRecord> NodeRecords { get; set; }
+
+        public NodeHashmap() {
+
+            this.NodeRecords = new Dictionary<NodeRecord, NodeRecord>();
+        }
+
+        public void Initialize() {
+            this.NodeRecords.Clear();
+        }
+
+        public int Count() {
+            return this.NodeRecords.Count;
+        }
+
+        public void AddToClosed(NodeRecord nodeRecord) {
+            this.NodeRecords.Add(nodeRecord, nodeRecord);
+        }
+
+        public void RemoveFromClosed(NodeRecord nodeRecord) {
+            this.NodeRecords.Remove(nodeRecord);
+        }
+
+        public NodeRecord SearchInClosed(NodeRecord nodeRecord) {
+            //here I cannot use the == comparer because the nodeRecord will likely be a different computational object
+            //and therefore pointer comparison will not work, we need to use Equals
+            //LINQ with a lambda expression
+
+            if (this.NodeRecords.ContainsKey(nodeRecord))
+                return NodeRecords[nodeRecord];
+
+            //  return this.NodeRecords.FirstOrDefault(n => n.Equals(nodeRecord));
+            return null;
+        }
+
+        public ICollection<NodeRecord> All() {
+            return this.NodeRecords.Values;
+
+        }
+    }
+}
