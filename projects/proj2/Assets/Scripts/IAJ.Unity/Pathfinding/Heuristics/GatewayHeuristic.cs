8d671bbb6baa4bd79b4360d674e7c1a1742ce556
Shinkirou <joaoromeirasamado@outlook.com>
2016-10-31 18:37:45 +0000

    more cleaning

--- projects/proj2/Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/GatewayHeuristic.cs
+++ projects/proj2/Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/GatewayHeuristic.cs
@@ -31,22 +31,13 @@ namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics {
                     foreach (Gateway endGateway in clusterEnd.gateways) {
                         float endDistance = EuclideanDistance(goalNode.LocalPosition, endGateway.center);
                         while (true) {
-                            //Debug.Log(1);
                             if (ClusterGraph.gateways[i].center == startGateway.center) {
-                                //Debug.Log(ClusterGraph.gateways[i].center);
-                                //Debug.Log(startGateway.center);
                                 break;
                             }
                             i++;
                         }
                         while (true) {
-                            //Debug.Log(1);
-                            //Debug.Log(ClusterGraph.gateways[j].center);
-                            //Debug.Log(endGateway.center);
                             if (ClusterGraph.gateways[j].center == endGateway.center) {
-                                //Debug.Log(1);
-                                //Debug.Log(ClusterGraph.gateways[j].center);
-                                //Debug.Log(endGateway.center);
                                 break;
                             }
                             j++;
@@ -63,36 +54,6 @@ namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics {
                 return finalDistance;
             }
         }
-
-
-        /*public float H(NavigationGraphNode node, NavigationGraphNode goalNode) {
-            float finalDistance = float.MaxValue;
-            Cluster clusterStart = ScriptableObject.CreateInstance<Cluster>();
-            Cluster clusterEnd = ScriptableObject.CreateInstance<Cluster>();
-            Cluster clusterTemp1 = ScriptableObject.CreateInstance<Cluster>();
-            Cluster clusterTemp2 = ScriptableObject.CreateInstance<Cluster>();
-            clusterStart = this.ClusterGraph.Quantize(node);
-            clusterEnd = this.ClusterGraph.Quantize(goalNode);
-            if ((object.ReferenceEquals(null, clusterStart)) || (object.ReferenceEquals(null, clusterEnd))) {
-                return EuclideanDistance(node.LocalPosition, goalNode.LocalPosition);
-            }
-            if (clusterStart.center == clusterEnd.center) {
-                return EuclideanDistance(node.LocalPosition, goalNode.LocalPosition);
-            } else {
-                foreach (Gateway startGateway in clusterStart.gateways) {
-                    while (true) {
-                        if (startGateway.clusters[0] == clusterStart) {
-                            clusterTemp2 = startGateway.clusters[1];
-                            clusterTemp1 = startGateway.clusters[0];
-                        } else {
-                            clusterTemp2 = startGateway.clusters[0];
-                            clusterTemp1 = startGateway.clusters[1];
-                        }
-                    }
-                }
-            }
-            return finalDistance;
-        } */
 
         public float EuclideanDistance(Vector3 startPosition, Vector3 endPosition) {
             return (endPosition - startPosition).magnitude;
