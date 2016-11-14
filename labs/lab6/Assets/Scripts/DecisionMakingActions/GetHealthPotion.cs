580bf490f57906fadec6b814940aba7db327fa0c
Axel Friberg <axl.friberg@gmail.com>
2016-11-14 16:56:22 +0000

    Added the actions from lab5 to lab6.

---
+++ labs/lab6/Assets/Scripts/DecisionMakingActions/GetHealthPotion.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.GameManager;
+using Assets.Scripts.IAJ.Unity.DecisionMaking.GOB;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.DecisionMakingActions
+{
+    public class GetHealthPotion : WalkToTargetAndExecuteAction
+    {
+        public GetHealthPotion(AutonomousCharacter character, GameObject target) : base("GetHealthPotion",character,target)
+        {
+        }
+
+        public override bool CanExecute()
+        {
+            if (!base.CanExecute())
+                return false;
+            else
+                return true;
+        }
+
+        public override bool CanExecute(WorldModel worldModel)
+        {
+            if (!base.CanExecute(worldModel))
+                return false;
+            else
+                return true;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            this.Character.GameManager.GetHealthPotion(this.Target);
+        }
+
+        public override void ApplyActionEffects(WorldModel worldModel)
+        {
+            base.ApplyActionEffects(worldModel);
+            worldModel.SetProperty(Properties.MANA, this.Character.GameManager.characterData.MaxHP);
+            //disables the target object so that it can't be reused again
+            worldModel.SetProperty(this.Target.name, false);
+        }
+    }
+}
